f95c5f92a01f734bfe71e31a770aa8f774c64b94
Juraj Slavicek <juraj.slavicek@gmail.com>
2025-07-07 16:26:35 +0200

    More robust Place model

--- place/admin.py
+++ place/admin.py
@@ -1,61 +1,9 @@
 from django.contrib import admin
-from django import forms
-from django.contrib.gis.geos import Point
 
 from place.models import Place
 
-
-from django import forms
-from django.contrib.gis.geos import Point
-from place.models import Place
-
-class PlaceForm(forms.ModelForm):
-    lat = forms.FloatField(required=True)
-    lon = forms.FloatField(required=True)
-
-    class Meta:
-        model = Place
-        exclude = ['lat','lon', 'location']
-        widgets = {
-            'location': forms.HiddenInput()
-        }
-
-    class Media:
-        js = ('admin/js/place_location_autofill.js',)
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # Pre-fill lat/lon from instance.location if editing
-        if self.instance and self.instance.location:
-            self.initial['lat'], self.initial['lon'] = self.instance.location.tuple
-
-    def clean(self):
-        cleaned_data = super().clean()
-        lat = cleaned_data.get('lat')
-        lon = cleaned_data.get('lon')
-        self.cleaned_data['location'] = Point(lon, lat)
-        return self.cleaned_data
-
-    def save(self, commit=True):
-        instance = super().save(commit=False)
-        instance.location = self.cleaned_data['location']
-        if commit:
-            instance.save()
-        return instance
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
-    list_display = ('name', 'location', 'google_maps_url', 'created_at', 'updated_at')
+    list_display = ('name', 'location',  'created_at', 'updated_at')
     readonly_fields = ('created_at', 'updated_at')
-    form = PlaceForm
-    
-    fieldsets = (
-        (None, {
-            'fields': ('name', 'type', ('lat', 'lon'), 'google_maps_url', 'website', 'note')
-        }),
-        ('Timestamps', {
-            'fields': ('created_at', 'updated_at'),
-            'classes': ('collapse',),
-            'description': 'Automatically managed timestamp fields'
-        }),
-    )
--- place/models.py
+++ place/models.py
@@ -1,19 +1,49 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 from django.contrib.gis.db import models as gis_models
+from django.db.models import OneToOneField
+
 from common.models import CreatedUpdatedModel
+from scraping.models import ScrapedPlace
+
 
 class Place(CreatedUpdatedModel):
     class PlaceType(models.TextChoices):
-        INDOOR_PLAYGROUND = "INDOOR_PLAYGROUND"
         PLAYGROUND = "PLAYGROUND"
-        RESTAURANT = "RESTAURANT"
-        FAMILY_SPOT= "FAMILY_SPOT"
-        CAFE = "CAFE"
+        INDOOR_PLAYGROUND = "INDOOR_PLAYGROUND"
         KINDERGARTEN = "KINDERGARTEN"
+        CAFE = "CAFE"
+        RESTAURANT = "RESTAURANT"
+        SHOP = "SHOP"
+        AMUSEMENT_PARK = "AMUSEMENT_PARK"
+        MUSEUM = "MUSEUM"
+        GALLERY = "GALLERY"
+        ZOO = "ZOO"
+        AQUAPARK = "AQUAPARK"
+        COMMUNITY_CENTER = "COMMUNITY_CENTER"
+        KIDS_PLAYROOM = "KIDS_PLAYROOM"
+        CASTLE = "CASTLE"
+        HOTEL = "HOTEL"
+        ATTRACTION = "ATTRACTION"
+        NATURAL_ATTRACTION = "NATURAL_ATTRACTION"
+
+    class PlaceSeasonType(models.TextChoices):
+        WINTER = "WINTER"
+        SUMMER = "SUMMER"
+        ALL = "ALL"
 
     name = models.CharField(max_length=255, null=False, blank=False)
+    scraped_id = OneToOneField(ScrapedPlace, on_delete=models.SET_NULL, null=True, blank=True)
+    type = ArrayField(models.CharField(max_length=255, choices=PlaceType.choices), default=list,db_index=True)
+    description = models.TextField(null=True, blank=True)
     location = gis_models.PointField(null=False, blank=False)
-    google_maps_url = models.URLField(max_length=255)
-    website = models.URLField(max_length=255)
-    note = models.TextField(max_length=255, null=True, blank=True)
-    type = models.CharField(max_length=255, choices=PlaceType.choices, null=False, blank=False)
+    country_code = models.CharField(max_length=255, null=False, blank=False)
+    city = models.CharField(max_length=255, null=False, blank=False)
+    min_age = models.IntegerField(null=True, blank=True)
+    max_age = models.IntegerField(null=True, blank=True)
+    website = models.URLField(max_length=255, null=True, blank=True)
+    note = models.TextField(null=True, blank=True)
+    season = models.CharField(max_length=255, choices=PlaceSeasonType.choices, null=True, blank=True)
+    is_admission_free = models.BooleanField(default=False)
+    is_visible = models.BooleanField(default=True,db_index=True)
+
